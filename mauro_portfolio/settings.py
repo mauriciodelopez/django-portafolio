87c73869e4b87356e1a911bb991845a41a812ccb
mauriciodelopez <94752287+mauriciodelopez@users.noreply.github.com>
2023-11-04 13:53:21 -0500

    new requirements.txt

--- mauro_portfolio/settings.py
+++ mauro_portfolio/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-_i_06=4p=f=qf&rbd_4)hq-tjt=a#z_b3-9ea)(*2lt6c2u6zq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
